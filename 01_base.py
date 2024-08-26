c0b2afb1b21921f4b5628b9e05fddba7c370dd05
Aleksandr Yurov <freelancer007@list.ru>
2024-08-26 21:50:08 +0300

    modul_1_3

---
+++ 01_base.py
@@ -0,0 +1,17 @@
+# 1st program
+print(9**0.5*5)
+
+# 2nd program
+print(9.99>9.98 and 1000 != 1000.1)
+
+# 3rd program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2) == (2*(2+2)))
+
+# 4th program
+i ='123.456'
+i = float(i) * 10
+i = int(i)%10
+print(i)
+
---
+++ modul_1_3.py
@@ -0,0 +1,24 @@
+name = 'Alex'
+print('Name:', name)
+
+age = 16
+print('Age:', age)
+age += 19
+print('New age:', age)
+
+is_student = True
+print('Is student:', is_student)
+
+# Я конечно не эксперт, но данное задание не совсем показывает смысл лекции, думаю ниже будет более правильное решение
+
+value = 'Alex'
+print('Name:', value)
+
+value = 16
+print('Age:', value)
+value += 19
+print('New age:', value)
+
+value = True
+print('Is student:', value)
+
