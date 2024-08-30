1f829e78127936921b49f86ce81ee84523af6b3c
alatricta <freelancer007@list.ru>
2024-08-30 19:05:59 +0300

    module 2 4

---
+++ module_2_4.py
@@ -0,0 +1,12 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+primes = []
+not_primes = []
+
+for i in range(1, len(numbers)):
+    num = numbers[i]
+    
+    for j in range
+
+print(primes)
+print(not_primes)
