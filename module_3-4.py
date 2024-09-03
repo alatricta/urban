dbd5d0bec7475b088f611efd9a9e1e005901652d
Aleksandr Yurov <47927194+alatricta@users.noreply.github.com>
2024-09-03 18:31:04 +0300

    Delete the file at 'module_3-4.py'

--- module_3-4.py
+++
@@ -1,7 +0,0 @@
-def single_root_words(root_word: str, *other_words):
-    same_words = [a for a in other_words if ((root_word in a) or (a in root_word))]
-    print(same_words)
-    
-    
-if __name__ == '__main__':
-    single_root_words('asd', 'vreg', 'asdfg', 'vfd', 'as')
