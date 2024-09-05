1aa2e4b42ca4d592dea0904b708826f8d0df33a7
alatricta <freelancer007@list.ru>
2024-09-05 12:29:40 +0300

    module 4 1

---
+++ module_4_fake_math.py
@@ -0,0 +1,5 @@
+def divide(first: int, second: int) -> float:
+    if second == 0:
+        return 'Ошибка!'
+    else:
+        return first / second
---
+++ module_4_true_math.py
@@ -0,0 +1,5 @@
+def divide(first: int, second: int) -> float:
+    if second == 0:
+        return float('inf')
+    else:
+        return first / second
