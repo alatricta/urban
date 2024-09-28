10adeb01fb037b1ed3ba2f3d2470389e11cf9e86
Aleksandr Yurov <freelancer007@list.ru>
2024-09-28 21:44:12 +0300

    Update module 6 hard

--- module_6_hard.py
+++ module_6_hard.py
@@ -4,12 +4,13 @@ import math
 class Figure:
     sides_count = 0
 
-    def __init__(self, color: tuple[int, int, int], *sides):
-        if len(sides) != self.sides_count:
-            self.__sides = tuple(1 for _ in range(self.sides_count))
-        else:
-            self.__sides = sides
-        self.__color = color
+    def __init__(self, color: list[int] | tuple[int], *sides: int):
+        # if len(sides) != self.sides_count:
+        #     self.__sides = tuple(1 for _ in range(self.sides_count))
+        # else:
+        #     self.__sides = sides
+        self.__color = [*color] if self.__is_valid_color(*color) else [0, 0, 0]
+        self.__sides = [*sides] if len(sides) == self.sides_count else [1] * self.sides_count
         self.filled = False
 
     def __len__(self):
@@ -19,16 +20,16 @@ class Figure:
         return self.__color
 
     def __is_valid_color(self, r: int, g: int, b: int) -> bool:
-        for num in (r,g,b):
-            if not (isinstance(num, int) and 0 <= num <= 255):
+        for num in (r, g, b):
+            if (not isinstance(num, int)) or (num < 0) or (num > 255):
                 return False
         return True
 
     def set_color(self, r: int, g: int, b: int) -> bool:
-        if not self.__is_valid_color(r,g,b):
+        if not self.__is_valid_color(r, g, b):
             # raise ValueError('Номера цветов должны быть в пределах от 0 до 255 включительно')
             return False
-        self.__color = (r, g, b)
+        self.__color = [r, g, b]
         return True
 
     def get_sides(self):
@@ -39,14 +40,14 @@ class Figure:
             return False
 
         for num in sides:
-            if not (isinstance(num, int) and num > 0):
+            if (not isinstance(num, int)) or (num < 0):
                 return False
         return True
 
     def set_sides(self, *new_sides) -> bool:
         if not self.__is_valid_sides(*new_sides):
             return False
-        self.__sides = new_sides
+        self.__sides = [*new_sides]
         return True
 
 
@@ -54,7 +55,7 @@ class Circle(Figure):
     sides_count = 1
 
     def __init__(self, color: tuple[int, int, int], *sides: int):
-        super().__init__(color,*sides)
+        super().__init__(color, *sides)
         self.__radius = self.get_sides()[0] / (2 * math.pi)
 
     def get_square(self):
@@ -64,22 +65,25 @@ class Circle(Figure):
 class Triangle(Figure):
     sides_count = 3
 
-    def __init__(self, color: tuple[int, int, int], *sides: int):
-        super().__init__(color,*sides)
+    # def __init__(self, color: tuple[int, int, int], *sides: int):
+    #     super().__init__(color, *sides)
 
     def get_square(self):
-        a,b,c = self.get_sides()
-        p = sum( (a,b,c) ) / 2
-        res = math.sqrt( p * (p - a) * (p-b) * (p - c) )
+        a, b, c = self.get_sides()
+        p = (a + b + c) / 2
+        res = math.sqrt(p * (p - a) * (p-b) * (p - c))
         return res
 
 
 class Cube(Figure):
     sides_count = 12
 
-    def __init__(self, color: tuple[int, int, int], side: int):
-        self.__sides = tuple(side for _ in range(self.sides_count))
-        super().__init__(color,*self.__sides)
+    def __init__(self, color: tuple[int, int, int], *sides: int):
+        if len(sides) > 1:
+            self.__sides = [1] * self.sides_count
+        else:
+            self.__sides = list(sides[0] for _ in range(self.sides_count))
+        super().__init__(color, *self.__sides)
 
     def get_volume(self):
         return self.__sides[0] ** 3
