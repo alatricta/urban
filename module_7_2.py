f2a032ecd63c770fe955259d91e155e46392fc2b
Aleksandr Yurov <freelancer007@list.ru>
2024-09-28 22:47:24 +0300

    module_7_2

---
+++ module_7_2.py
@@ -0,0 +1,24 @@
+def custom_write(file_name, strings):
+    f = open(file_name, 'w', encoding='utf-8')
+    strings_positions = {}
+    for number_of_line, line in enumerate(strings):
+        strings_positions[(number_of_line+1, f.tell())] = line
+        f.write(f'{line}\n')
+    return strings_positions
+
+
+def main():
+    info = [
+        'Text for tell.',
+        'Используйте кодировку utf-8.',
+        'Because there are 2 languages!',
+        'Спасибо!'
+        ]
+
+    result = custom_write('test.txt', info)
+    for elem in result.items():
+        print(elem)
+
+
+if __name__ == "__main__":
+    main()
