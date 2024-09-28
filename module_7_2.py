4bf02082153c67e18c6613d46a8f35f4c7861c3a
Aleksandr Yurov <freelancer007@list.ru>
2024-09-28 22:51:14 +0300

    Update module_7_2

--- module_7_2.py
+++ module_7_2.py
@@ -1,9 +1,12 @@
 def custom_write(file_name, strings):
-    f = open(file_name, 'w', encoding='utf-8')
-    strings_positions = {}
-    for number_of_line, line in enumerate(strings):
-        strings_positions[(number_of_line+1, f.tell())] = line
-        f.write(f'{line}\n')
+    # f = open(file_name, 'w', encoding='utf-8')
+    with open(file_name, 'w', encoding='utf-8') as f:
+        strings_positions = {}
+        for number_of_line, line in enumerate(strings):
+            strings_positions[(number_of_line+1, f.tell())] = line
+            f.write(f'{line}\n')
+    # f.close()
+
     return strings_positions
 
 
